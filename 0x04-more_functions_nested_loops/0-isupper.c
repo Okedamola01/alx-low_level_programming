dcec17ea69dd4a0f78101f5b859a2cf81a56d463
Oke Damola <okedamola01@gmail.com>
2022-07-17 05:47:31 -0700

    2-mul.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -9,13 +9,13 @@ int _isupper(int c)
 {
 char uppercase = 'A';
 int isupper = 0;
-for (;uppercase <= 'Z'; uppercase++)
+for (; uppercase <= 'Z'; uppercase++)
 {
-if(c == uppercase)
+if (c == uppercase)
 {
 isupper = 1;
 break;
 }
 }
-return(isupper);
+return (isupper);
 }

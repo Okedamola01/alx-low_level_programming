dce8c8956161d8ed3a5e21066012e303882e8368
Oke Damola <okedamola01@gmail.com>
2022-07-27 10:12:28 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,8 +7,9 @@
  * Return: Always 0
  */
 
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc, char *argv[])
 {
+	if (argc > 0)
 	printf("%s\n", argv[0]);
-	return 0;
+	return (0);
 }

c35c8521fc3f8c0b6d4bc15d60349bf9ebee585c
okedamola01 <okedamola01@gmail.com>
2022-10-07 09:32:50 -0700

    yes

---
+++ 0x18-dynamic_libraries/100-atoi.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _atoi - converts a string to an integer
+ * @: char type string
+ * Return: integer converted
+ */
+
+int _atoi(char *s)
+{
+	int sign = 1, resp = 0, firstnum;
+
+	for (firstNum = 0; !(s[firstNum] >= 48 && s[firstNum] <= 57); firstNum++)
+	{
+		if (s[firstNum] == '-')
+		{
+			sign *= -1;
+		}
+	}
+
+	for (int i = firstNum; s[i] >= 48 && s[i] <= 57; i++)
+	{
+		resp *= 10;
+		resp += (s[i] - 48);
+	}
+	return (sign * resp);
+}
